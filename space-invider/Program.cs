46944903d385b1a04aec9ee3e2bd66aa6e100a38
wolframowsky <wolframowsky@gmail.com>
2019-10-21 22:30:03 +0200

    Next commit

--- space-invider/Program.cs
+++ space-invider/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace space_invider
 {
@@ -7,64 +8,12 @@ namespace space_invider
     {
         static void Main(string[] args)
         {
-
-            Ship ship = new Ship(13, 18);
-            Frame frame = new Frame(15, 20);
-
-
-            /*
-                        while (true)
-            {
-                Draw(ship,frame);
-
-                ConsoleKeyInfo keyPressed = Console.ReadKey();
-
-                if ((keyPressed.Key == ConsoleKey.W && ship.Y != 1) || (keyPressed.Key == ConsoleKey.S && ship.Y != frame.Height-2))
-                {
-                    ship.Y += (keyPressed.Key == ConsoleKey.S) ? 1 : -1;
-                }
-                if ((keyPressed.Key == ConsoleKey.A && ship.X != 2) || (keyPressed.Key == ConsoleKey.D && ship.X != frame.Width - 1))
-                {
-                    ship.Y += (keyPressed.Key == ConsoleKey.D) ? 1 : -1;
-                }
-                
-            }
-
-             */
-
-            frame.CreateFrame();
-            Console.Clear();
-
-
-
-
-
+            Console.CursorVisible = false;
+            GameBoard gB = new GameBoard();
+            gB.Play();
 
         }
 
-        static void Draw(Ship ship,Frame frame)
-        {
-            Console.Clear();
-            frame.CreateFrame();
-
-            Console.WriteLine("(" + ship.X +"," + ship.Y +")");
-
-            for (int y = 1; y < frame.Height; y++)
-            {
-                for(int x =1; x<frame.Width;x++)
-                {
-                    if (x == ship.X && y == ship.Y)
-                    {
-                        Console.SetCursorPosition(x, y);
-                        Console.Write(ship.SpaceShip);
-                    }
-                        
-                    
-                }
-            }
-
-
-        }
     }
 
 
@@ -120,11 +69,95 @@ namespace space_invider
             this.SpaceShip = '^';
         }
 
+    }
 
+    class Laser: Position
+    {
+        public char FireLasor { get; set; }
+
+        public Laser(int x, int y):base(x,y)
+        {
+            this.FireLasor = '|';
+        }
+
+        public void MoveLasor(GameBoard gb)
+        {
+            while (this.Y != 1)
+            {
+                this.Y--;
+                Console.SetCursorPosition(this.X, this.Y);
+                Console.Write(this.FireLasor);
+                Thread.Sleep(10);
+                gb.Draw();
+                
+            }
+        }
     }
 
     class GameBoard
     {
+        
+        public Ship ship { get; set; }
+        public Frame frame { get; set; }
+
+        public GameBoard()
+        {
+            this.ship = new Ship(13, 18);
+            this.frame = new Frame(15, 20);
+        }
+
+        public void Play() 
+        {
+            Laser laser;
+
+            while (true)
+            {
+                Draw();
+
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                if ((keyPressed.Key == ConsoleKey.W && ship.Y != 1) || (keyPressed.Key == ConsoleKey.S && ship.Y != frame.Height - 2))
+                {
+                    ship.Y += (keyPressed.Key == ConsoleKey.S) ? 1 : -1;
+                }
+                if ((keyPressed.Key == ConsoleKey.A && ship.X != 1) || (keyPressed.Key == ConsoleKey.D && ship.X != frame.Width - 2))
+                {
+                    ship.X += (keyPressed.Key == ConsoleKey.D) ? 1 : -1;
+                }
+                if (keyPressed.Key == ConsoleKey.Spacebar)
+                {
+
+                    laser = new Laser(ship.X, ship.Y);
+                    laser.MoveLasor(this);
+                }
+
+
+            }
+
+        }
+
+        public void Draw()
+        {
+            Console.Clear();
+            frame.CreateFrame();
+
+            Console.WriteLine("(" + ship.X + "," + ship.Y + ")");
+
+            for (int y = 1; y < frame.Height; y++)
+            {
+                for (int x = 1; x < frame.Width; x++)
+                {
+                    if (x == ship.X && y == ship.Y)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(ship.SpaceShip);
+                    }
+
+
+
+                }
+            }
+        }
     }
 
     
