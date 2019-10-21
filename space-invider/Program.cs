6f21f27e8ac2ec41c32cb87ff3578ab4d60f5663
Konrad Wilczek <wolframowsky@gmail.com>
2019-10-21 19:11:59 +0200

    Added Frame

--- space-invider/Program.cs
+++ space-invider/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace space_invider
 {
@@ -6,7 +7,80 @@ namespace space_invider
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Frame frame = new Frame(30,40);
+            frame.CreateFrame();
+
+            frame.DrawFrame();
+
+
+          
         }
     }
+
+
+    class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    class FrameElement : Position
+    {
+        public char Frame { get; set; }
+
+        public FrameElement(int y, int x) : base(x,y)
+        { 
+            this.Frame = '#';
+        }
+    }
+    class Frame
+    {
+        public List<FrameElement> frameList = new List<FrameElement>();
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Frame(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void CreateFrame()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    if (i == 0 || i==this.Height-1 || j==0 || j==this.Width-1)
+                    {
+                        frameList.Add(new FrameElement(i, j));
+                    }
+                    
+                }
+            }
+        }
+
+        public void DrawFrame()
+        {
+            foreach (var fl in frameList)
+            {
+                Console.SetCursorPosition(fl.X, fl.Y);
+                Console.Write(fl.Frame);
+            }
+        }
+
+    }
+    class GameBoard
+    {
+    }
+
+    
+
 }
